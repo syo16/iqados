13dc5c2bddca1f97608a98b5349f6610bb313aaa
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-05 21:52:54 +0900

    chapter8 マウスの解読1

--- bootpack.c
+++ bootpack.c
@@ -40,6 +40,7 @@ void HariMain(void)
     struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i, j;
+    unsigned char mouse_dbuf[3], mouse_phase;
 
     init_gdtidt();
     init_pic();
@@ -61,7 +62,9 @@ void HariMain(void)
     fifo8_init(&mousefifo, 128, mousebuf);
 
     init_keyboard();
+
     enable_mouse();
+    mouse_phase = 0; /* マウスの0xfaを待っている段階へ */
 
 	for (;;) {
         io_cli();
@@ -77,9 +80,29 @@ void HariMain(void)
             } else if (fifo8_status(&mousefifo) != 0) {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                sprintf(s, "%x", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                if (mouse_phase == 0) {
+                    /* マウスの0xfaを待っている段階 */
+                    if (i == 0xfa) {
+                        mouse_phase = 1;
+                    }
+                } else if (mouse_phase == 1) {
+                    /* マウスの１バイト目を待っている段階 */
+                    mouse_dbuf[0] = i;
+                    mouse_phase = 2;
+                } else if (mouse_phase == 2) {
+                    /* マウスの２バイト目を待っている段階 */
+                    mouse_dbuf[1] = i;
+                    mouse_phase = 3;
+                } else if (mouse_phase == 3) {
+                    /* マウスの２バイト目を待っている段階 */
+                    mouse_dbuf[2] = i;
+                    mouse_phase = 1;
+                    /* データが３バイト揃ったので表示 */
+                    sprintf(s, "%x %x %x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                }
+
             }
         }
 	}

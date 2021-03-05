913b96b4641d6019a04d0789fa7a2a49fb3d3d0f
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-05 13:41:17 +0900

    chapter5 GDTとIDTの初期化

--- bootpack.c
+++ bootpack.c
@@ -17,6 +17,24 @@
 #define COL8_008484     14
 #define COL8_848484     15
 
+struct BOOTINFO {
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+};
+
+struct SEGMENT_DESCRIPTOR {
+    short limit_low, base_low;
+    char base_mid, access_right;
+    char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+    short offset_low, selector;
+    char dw_count, access_right;
+    short offset_high;
+};
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -34,13 +52,13 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, char *s);
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0 ,int py0, char *buf, int bxsize);
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
 extern void sprintf(char *str, char *fmt, ...);     // mysprintf.c で、独自の sprintf を作成したので行追加
 
-struct BOOTINFO {
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;
-};
 
 
 void HariMain(void)
@@ -209,5 +227,49 @@ void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0 ,int py
             vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
         }
     }
+    return;
+}
+
+void init_gdtidt(void) {
+    struct SEGMENT_DESCRIPTOR   *gdt = (struct SEGMENT_DESCRIPTOR   *) 0x00270000;
+    struct GATE_DESCRIPTOR      *idt = (struct GATE_DESCRIPTOR      *) 0x0026f800;
+    int i;
+
+    /* GDTの初期化　*/
+    for (i = 0; i < 8192; i++) {
+        set_segmdesc(gdt + i, 0, 0, 0);
+    }
+    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+    load_gdtr(0xffff, 0x00270000);
+
+    /* IDTの初期化 */
+    for (i = 0; i < 256; i++) {
+        set_gatedesc(idt + i, 0, 0, 0);
+    }
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar) {
+    if (limit > 0xfffff) {
+        ar |= 0x80000; /* G_bit = 1 */
+        limit /= 0x1000;
+    }
+    sd->limit_low       = limit & 0xffff;
+    sd->base_low        = base & 0xffff; 
+    sd->base_mid        = (base >> 16) & 0xff;
+    sd->access_right    = ar & 0xff;
+    sd->limit_high      = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high       = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar) {
+    gd->offset_low      = offset & 0xffff;
+    gd->selector        = selector;
+    gd->dw_count        = (ar >> 8) & 0xff;
+    gd->access_right    = ar & 0xff;
+    gd->offset_high     = (offset >> 16) & 0xffff;
     return;
 }

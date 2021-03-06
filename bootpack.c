5f34fe5ea8122291f09f32f83b385b295a193d5f
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-06 13:23:07 +0900

    chapter9 メモリ管理

--- bootpack.c
+++ bootpack.c
@@ -13,7 +13,11 @@ void HariMain(void)
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
 
+    unsigned int memtotal;
+
     struct MOUSE_DEC mdec;
+
+    struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
 
     init_gdtidt();
     init_pic();
@@ -35,8 +39,12 @@ void HariMain(void)
     init_keyboard();
     enable_mouse(&mdec);
 
-    i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-    sprintf(s, "memory %dMB", i);
+    memtotal = memtest(0x00400000, 0xbfffffff);
+    memman_init(memman);
+    memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x009efff */
+    memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+    sprintf(s, "memory %dMB     free: %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
 	for (;;) {
@@ -124,5 +132,108 @@ unsigned int memtest(unsigned int start, unsigned int end)
 	}
 
 	return i;
+}
+
+
+void memman_init(struct MEMMAN *man) {
+    man->frees = 0; /* 空き情報の個数 */
+    man->maxfrees = 0; /* 状況観察用: freesの最大値 */
+    man->lostsize = 0; /* 解放に失敗した合計サイズ */
+    man->losts = 0; /* 解放に失敗した回数 */
+    return;
+}
+
+unsigned int memman_total(struct MEMMAN *man) {
+/* 空きサイズの合計を報告 */
+    unsigned int i, t = 0;
+    for (i = 0; i < man->frees; i++) {
+        t += man->free[i].size;
+    }
+    return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size) {
+/* 確保 */
+    unsigned int i, a;
+
+    for (i = 0; i < man->frees; i++) {
+        if (man->free[i].size >= size) {
+            /* 十分な広さの空きを発見 */
+            a = man->free[i].addr;
+            man->free[i].addr += size;
+            man->free[i].size -= size;
+            if (man->free[i].size == 0) {
+                /* free[i]がなくなったので前へつめる */
+                man->frees--;
+                for (; i < man->frees; i++) {
+                    man->free[i] = man->free[i + 1]; /* 構造体の代入 */
+                }
+            }
+            return a;
+        }
+    }
+    return 0; /* 空きがない */
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size) {
+/* 解放 */
+    int i, j;
+    /* まとめやすさを考えると、free[]がaddr順に並んでいる方がいい */
+    /* だからまず、どこに入れるべきか決める */
+    for (i = 0; i < man->frees; i++) {
+        if (man->free[i].addr > addr) {
+            break;
+        }
+    }
+    /* free[i - 1].addr < addr < free[i].addr */
+    if (i > 0) {
+        /* 前がある */
+        if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+            /* 前の空き領域にまとめられる */
+            man->free[i - 1].size += size;
+            if (i < man->frees) {
+                /* 後ろもある */
+                if (addr + size == man->free[i].addr) {
+                    /* なんと後ろもまとめられる */
+                    man->free[i - 1].size += man->free[i].size;
+                    /* man->free[i]の削除 */
+                    /* free[i]がなくなったので前へつめる */
+                    man->frees--;
+                    for (; i < man->frees; i++) {
+                        man->free[i] = man->free[i+1]; /* 構造体の代入 */
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+    /* 前とはまとめられなかった */
+    if (i < man->frees) {
+        /* 後ろがある */
+        if (addr + size == man->free[i].addr) {
+            /* 後ろとはまとめられる */
+            man->free[i].addr = addr;
+            man->free[i].size += size;
+            return 0; /* 成功終了 */
+        }
+    }
+    /* 前にも後ろにもまとめられない */
+    if (man->frees < MEMMAN_FREES) {
+        /* free[i]より後ろを、後ろへずらして、隙間を作る */
+        for (j = man->frees; j > i; j--) {
+            man->free[j] = man->free[j - 1];
+        }
+        man->frees++;
+        if (man->maxfrees < man->frees) {
+            man->maxfrees = man->frees; /* 最大値を更新 */
+        }
+        man->free[i].addr = addr;
+        man->free[i].size = size;
+        return 0; /* 成功終了 */
+    }
+    /* 後ろにずらせなかった */
+    man->losts++;
+    man->lostsize += size;
+    return -1; /* 失敗終了 */
 }
 

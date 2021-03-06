1d20882642f2585a727bd81f41c794cccabce82b
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-06 16:22:09 +0900

    chapter10 memory.cに新しい関数の追加

--- bootpack.h
+++ bootpack.h
@@ -141,7 +141,7 @@ void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 void inthandler2c(int *esp);
 
-/* bootpack.c */
+/* memory.c */
 #define EFLAGS_AC_BIT           0x00040000
 #define CR0_CACHE_DISABLE       0x60000000
 
@@ -164,3 +164,5 @@ void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);

d15e885b13ff528afb83470c66c51f2ddf1ac56b
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-07 11:54:14 +0900

    chapter12 タイマーの導入

--- bootpack.h
+++ bootpack.h
@@ -20,6 +20,7 @@ int io_load_eflags(void);
 void io_store_eflags(int eflags);
 void load_gdtr(int limit, int addr);
 void load_idtr(int limit, int addr);
+void asm_inthandler20(void);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
@@ -193,6 +194,13 @@ void sheet_slide(struct SHEET *sht, int vx0, int vy0);
 void sheet_free(struct SHEET *sht);
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1);
 void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0); 
+
+/* timer.c */
+#define PIT_CTRL        0x0043
+#define PIT_CNT0        0x0040
+
+void init_pit(void);
+void inthandler20(int *esp);
 
 /* bootpack.c */
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title);

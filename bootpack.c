c89a7ba88945d312e3c76d2d498e578bcbd6cccd
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-10 11:25:52 +0900

    chapter18 Enterキーに対応

--- bootpack.c
+++ bootpack.c
@@ -230,6 +230,11 @@ void HariMain(void)
                     wait_KBC_sendready();
                     io_out8(PORT_KEYDAT, keycmd_wait);
                 }
+                if (i == 256 + 0x1c) { /* Enter */
+                    if (key_to != 0) { /* コンソールへ */
+                        fifo32_put(&task_cons->fifo, 10 + 256);
+                    }
+                }
                 /* カーソルの再表示 */
                 if (cursor_c >= 0) {
                     boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
@@ -378,7 +383,7 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
 void console_task(struct SHEET *sheet) {
     struct TIMER *timer;
     struct TASK *task = task_now();
-    int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
+    int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
     char s[2];
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -415,7 +420,7 @@ void console_task(struct SHEET *sheet) {
                 cursor_c = COL8_FFFFFF;
             }
             if (i == 3) { /* カーソルOFF */
-                boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, 28, cursor_x + 7, 43);
+                boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
                 cursor_c = -1;
             }
             if (256 <= i && i <= 511) { /* キーボードデータ(タスクA経由) */
@@ -423,8 +428,18 @@ void console_task(struct SHEET *sheet) {
                     /* バックスペース */
                     if (cursor_x > 16) {
                         /* カーソルをスペースで消してから、カーソルを一つ戻す */
-                        putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, " ", 1);
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
                         cursor_x -= 8;
+                    }
+                } else if (i == 10 + 256) {
+                    /* Enter */
+                    if (cursor_y < 28 + 112) {
+                        /* カーソルをスペースで消す */
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+                        cursor_y += 16;
+                        /* プロンプト表示 */
+                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">", 1);
+                        cursor_x = 16;
                     }
                 } else {
                     /* 一般文字 */
@@ -432,16 +447,16 @@ void console_task(struct SHEET *sheet) {
                         /* 一文字表示してから、カーソルを一つすすめる */
                         s[0] = i - 256;
                         s[1] = 0;
-                        putfonts8_asc_sht(sheet, cursor_x, 28, COL8_FFFFFF, COL8_000000, s, 1);
+                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
                         cursor_x += 8;
                     }
                 }
             }
             /* カーソル再表示 */
             if (cursor_c >= 0) {
-                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
             }
-            sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
+            sheet_refresh(sheet, cursor_x, cursor_y, cursor_x + 8, cursor_y + 16);
         }
     }
 }

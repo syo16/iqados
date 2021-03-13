7c206efbe1645d27d954c3ae6e43732cc248d3d6
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2021-03-13 11:18:41 +0900

    chapter25 コンソールを増やす

--- console.c
+++ console.c
@@ -10,9 +10,10 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
     cons.cur_x = 8;
     cons.cur_y = 28;
     cons.cur_c = -1;
-    *((int *) 0x0fec) = (int) &cons;
+    task->cons = &cons;
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
+    cons.timer = timer_alloc();
     timer_init(cons.timer, &task->fifo, 1);
     timer_settime(cons.timer, 50);
     file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
@@ -267,13 +268,13 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline) {
             datsiz  = *((int *) (p + 0x0010));
             dathrb  = *((int *) (p + 0x0014));
             q = (char *) memman_alloc_4k(memman, segsiz);
-            *((int *) 0xfe8) = (int) q;
-            set_segmdesc(gdt + 1003, finfo->size - 1,   (int) p, AR_CODE32_ER + 0x60);
-            set_segmdesc(gdt + 1004, segsiz - 1,        (int) q, AR_DATA32_RW + 0x60);
+            task->ds_base = (int)q;
+            set_segmdesc(gdt + task->sel / 8 + 1000, finfo->size - 1,   (int) p, AR_CODE32_ER + 0x60);
+            set_segmdesc(gdt + task->sel / 8 + 2000, segsiz - 1,        (int) q, AR_DATA32_RW + 0x60);
             for (i = 0; i < datsiz; i++) {
                 q[esp + i] = p[dathrb + i];
             }
-            start_app(0x1b, 1003 * 8, esp, 1004 * 8, &(task->tss.esp0));
+            start_app(0x1b, task->sel + 1000 * 8, esp, task->sel + 2000 * 8, &(task->tss.esp0));
             shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
             for (i = 0; i < MAX_SHEETS; i++) {
                 sht = &(shtctl->sheets0[i]);
@@ -311,9 +312,9 @@ void cons_putstr1(struct CONSOLE *cons, char *s, int l) {
 }
 
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax) {
-    int ds_base = *((int *) 0xfe8);
     struct TASK *task = task_now();
-    struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+    int ds_base = task->ds_base;
+    struct CONSOLE *cons = task->cons;
     struct SHTCTL *shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
     struct SHEET *sht;
     int *reg = &eax + 1; /* eaxの次の番号 */
@@ -334,8 +335,10 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
         sht->flags |= 0x10;
         sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
         make_window8((char *) ebx + ds_base, esi, edi, (char *) ecx + ds_base, 0);
-        sheet_slide(sht, 100, 50);
-        sheet_updown(sht, 3); /* 3という高さはtask_aの上 */
+        //sheet_slide(sht, 100, 50);
+        //sheet_updown(sht, 3); /* 3という高さはtask_aの上 */
+        sheet_slide(sht, (shtctl->xsize - esi) / 2, (shtctl->ysize - edi) / 2);
+        sheet_updown(sht, shtctl->top); /* 今のマウスと同じ高さになるように指定: マウスはこの上になる */
         reg[7] = (int) sht;
     } else if (edx == 6) {
         sht = (struct SHEET *) (ebx & 0xfffffffe);
@@ -432,8 +435,8 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 }
 
 int * inthandler0c(int *esp) {
-    struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
     struct TASK *task = task_now();
+    struct CONSOLE *cons = task->cons;
     char s[30];
     cons_putstr0(cons, "\nINT 0C :\n Stack Exception.\n");
     sprintf(s, "EIP = %x\n", esp[11]);
@@ -442,8 +445,8 @@ int * inthandler0c(int *esp) {
 }
 
 int *inthandler0d(int *esp) {
-    struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
     struct TASK *task = task_now();
+    struct CONSOLE *cons = task->cons;
     char s[30];
     cons_putstr0(cons, "\nINT 0d :\n GEneral Protected Exception.\n");
     sprintf(s, "EIP = %x\n", esp[11]);
